d82ac12d0c9269f6d6ab8ebcba763e0b1364a254
chitramsst <chitra.msst@gmail.com>
2024-05-26 22:09:58 +0530

    height setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -118,20 +118,22 @@
           <!-- item section -->
           <div class="h-[93%] p-7 w-full grid grid-cols-4 gap-10 ">
             <div v-for="n in 8" :key="n"
-              class="flex flex-col space-y-1 border-[0.5px] border-border rounded-lg px-3 pt-3">
-              <img
-                src="https://images.meesho.com/images/ratings_reviews/2541941610/2558399344/2541941610_2558399344_2de3b2ae89d32_512.jpeg"
-                class="max-h-[180px] w-[300px] rounded-lg resize shadow-xl" />
-              <div class="pt-2">
+              class="flex flex-col space-y-1 border-[0.5px] border-border rounded-lg px-3 pt-3 max-h-[350px]">
+              <div class="h-6/12">
+                <img
+                  src="https://cdn.shopify.com/s/files/1/2303/2711/files/2_e822dae0-14df-4cb8-b145-ea4dc0966b34.jpg?v=1617059123"
+                  class="min-h-[180px] max-h-[280px] w-[300px] rounded-lg resize shadow-xl" />
+              </div>
+              <div class="pt-2 h-2/12">
                 <p class=" text-sm"> Whity Giant Plants</p>
                 <p class="text-xs text-gray-400"> Artificial potted, indoor/outdoor Whitley Giant, 9cm</p>
               </div>
-              <div class="flex justify-between items-center">
+              <div class="flex justify-between items-center h-1/12">
                 <p class="font-bold text-xs text-primary"> $50 </p>
                 <p class="text-xs text-gray-400 border border-border/20 bg-border/20 px-3 py-1 rounded-full">
                   53 items</p>
               </div>
-              <div class="pt-2">
+              <div class="pt-2 h-2/12">
                 <p
                   class="  py-2 px-3 text-wrap border border-border rounded-lg flex items-center justify-center text-xs">
                   <i class="fas fa-plus pr-2"></i> Add To Cart
@@ -145,31 +147,31 @@
         <div class="w-3/12 p-5 h-full">
           <div class="h-[65%] w-full pb-5">
             <p class="font-bold pb-5"> Detail Items </p>
-            <div class="h-5/6 overflow-y-auto">
-            <div class="pt-3 flex flex-row space-x-2 pb-3" v-for="n in 2" :key="n">
-              <img
-                src="https://images.meesho.com/images/ratings_reviews/2541941610/2558399344/2541941610_2558399344_2de3b2ae89d32_512.jpeg"
-                class="max-h-[100px] w-[100px] rounded-lg resize shadow-xl" />
-              <div class="flex flex-col space-y-2 w-2/4">
-                <p class=" text-sm"> Whity Giant Plants</p>
-                <p class="text-xs text-gray-400"> Artificial potted, indoor/outdoor Whitley Giant, 9cm</p>
-                <p class="font-bold text-xs text-primary"> $50 </p>
-              </div>
-              <div class="flex flex-row space-x-1 justify-center items-center w-1/4">
-                <p
-                  class=" text-gray-500 text-wrap border-[0.5px]  border-border  rounded-lg flex items-center justify-center gap-3 text-md  h-7 w-7" @click="decrement(n)">
-                  <i class="fas fa-minus"></i>
-                </p>
-                <p class=" text-gray-500 text-wrap  flex items-center justify-center text-sm h-7 w-7">
-                  {{ this.count[n] }}
-                </p>
-                <p
-                  class=" text-white text-wrap  bg-primary rounded-lg flex items-center justify-center gap-3 text-md m-3 h-7 w-7" @click="increment(n)">
-                  <i class="fas fa-plus"></i>
-                </p>
+            <div class="h-[90%] overflow-y-auto">
+              <div class="pt-3 flex flex-row space-x-2 pb-3" v-for="n in 7" :key="n">
+                <img
+                  src="https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2022/07/essential-props-fstoppers-helenarosephotography-080-edit1.jpg"
+                  class="max-h-[100px] w-[100px] rounded-lg resize shadow-xl" />
+                <div class="flex flex-col space-y-2 w-2/4">
+                  <p class=" text-sm"> Whity Giant Plants</p>
+                  <p class="text-xs text-gray-400"> Artificial potted, indoor/outdoor Whitley Giant, 9cm</p>
+                  <p class="font-bold text-xs text-primary"> $50 </p>
+                </div>
+                <div class="flex flex-row space-x-1 justify-center items-center w-1/4">
+                  <p class=" text-gray-500 text-wrap border-[0.5px]  border-border  rounded-lg flex items-center justify-center gap-3 text-md  h-7 w-7"
+                    @click="decrement(n)">
+                    <i class="fas fa-minus"></i>
+                  </p>
+                  <p class=" text-gray-500 text-wrap  flex items-center justify-center text-sm h-7 w-7">
+                    {{ this.count[n] }}
+                  </p>
+                  <p class=" text-white text-wrap  bg-primary rounded-lg flex items-center justify-center gap-3 text-md m-3 h-7 w-7"
+                    @click="increment(n)">
+                    <i class="fas fa-plus"></i>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
           </div>
           <div class="h-[30%] rounded-lg bg-border/30 w-full flex flex-col space-y-2 p-5">
             <p class="font-bold"> Detail Payment </p>
@@ -210,34 +212,33 @@ export default {
     return {
       menuOpen: false,
       count: new Array(10).fill(1),
-      authStore : useAuthStore()
+      authStore: useAuthStore()
     };
   },
-  created(){
-    
+  created() {
+
   },
   methods: {
     toggleMenu() {
       this.menuOpen = !this.menuOpen;
     },
     increment(n) {
-      this.count[n] = this.count[n]+1
+      this.count[n] = this.count[n] + 1
     },
     decrement(n) {
-      if(this.count[n]==1) {
+      if (this.count[n] == 1) {
         this.count[n] = 1;
         return;
       }
-      if(this.count[n]>1) {
-        this.count[n]=this.count[n]-1;
+      if (this.count[n] > 1) {
+        this.count[n] = this.count[n] - 1;
       }
-      
+
     },
-		logout()
-		{
-			this.authStore.$reset()
-			this.$router.push({name : 'login'})
-		}
+    logout() {
+      this.authStore.$reset()
+      this.$router.push({ name: 'login' })
+    }
   }
 };
 </script>
